1fe02e5bc8c00c86cfd432f4f10c664ba67f8695
Itay Karavani <itaygrx@gmail.com>
2022-02-24 13:40:28 +0200

    mo

--- Server.py
+++ Server.py
@@ -5,57 +5,71 @@ import threading
 SERVERADDR = ("", 55000)
 SIZE = 1024
 
-clients = []
+# clients = []
 active_sockets = {}
 
 
-def client_main(client_sock, client_addr):
-    global clients
+def remove_socket(cl):
+    for name, sock in active_sockets:
+        if cl is sock:
+            broadcast(f"{name} left the chat.\n")
+            del active_sockets[name]
+
+
+def broadcast(msg):
+    for v in active_sockets.values():
+        v.send(msg.encode('utf-8'))
+
+
+def client_main(client_sock: socket.socket, client_addr):
     global active_sockets
 
     print(f"New Connection {client_addr} connected successfully.\n")
-    client_sock.send("OK@Connected Succesfully.\n".encode('utf-8'))
+    client_sock.send("LOGIN@Connected Succesfully.\n".encode('utf-8'))
 
     # LOGIN@Itay
     while True:
-        msg = client_sock.recv(SIZE).decode('utf-8').split("@")
-        cmd = msg[0]
+        try:
+            msg = client_sock.recv(SIZE).decode('utf-8')
+            cmd = msg[:msg.find("@")]
+            data = msg[msg.find("@")+1:]
+            print(msg,cmd,data)
 
-        if cmd == "DISCONNECT":
-            break
+            if cmd == "DISCONNECT":
+                client_sock.close()
+                remove_socket(client_sock)
+                break
 
-        elif cmd == "LOGIN":
-            name = msg[1]
-            if name in clients:
-                client_sock.send("Error@User name already exists in the system\n".encode('utf-8'))
-            else:
-                client_sock.send("OK@Logged In\n".encode('utf-8'))
-                clients.append(name)
-                active_sockets[name] = client_sock
+            elif cmd == "LOGIN":
+                name = data
+                if name in active_sockets.keys():
+                    client_sock.send("Error@User name already exists in the system\n".encode('utf-8'))
+                else:
+                    client_sock.send("OK@Logged In\n".encode('utf-8'))
+                    active_sockets[name] = client_sock
 
-        elif cmd == "SHOWFILES":
-            msg = "SHOWFILES@"
-            msg += '\n'.join(file for file in os.listdir(""))
-            client_sock.send(msg.encode('utf-8'))
+            elif cmd == "SHOWFILES":
+                msg = "SHOWFILES@"
+                msg += '\n'.join(file for file in os.listdir(""))
+                client_sock.send(msg.encode('utf-8'))
 
-        elif cmd == "SHOWUSERS":
-            msg = "SHOWUSERS@"
-            msg += '\n'.join(c for c in clients)
-            client_sock.send(msg.encode('utf-8'))
+            elif cmd == "SHOWUSERS":
+                msg = "SHOWUSERS@"
+                msg += '\n'.join(c for c in active_sockets.keys())
+                client_sock.send(msg.encode('utf-8'))
 
-        elif cmd == "DOWNLOAD":
-            pass
+            elif cmd == "DOWNLOAD":
+                pass
 
-        elif cmd == "MSG":
-            for curr in clients:
-                sock: socket.socket = active_sockets[curr[0]]
-                sock.send("MSG@".join(m + "@" for m in msg[1:]).encode('utf-8'))
+            elif cmd == "MSG":
+                broadcast(msg[1:])
 
-        elif cmd == "PMSG":
-            name = msg[1]
-            active_sockets[name].send("PMSG@".join(m + "@" for m in msg[2:]).encode('utf-8'))
+            elif cmd == "PMSG":
+                name = msg[1]
+                active_sockets[name].send("PMSG@".join(m + "@" for m in msg[2:]).encode('utf-8'))
 
-    print(f"{addr} Disconnected from server.\n")
+        except:
+            print(f"{addr} Disconnected from server.\n")
 
 
 if __name__ == '__main__':
@@ -69,3 +83,4 @@ if __name__ == '__main__':
         client, addr = server.accept()
         thread = threading.Thread(target=client_main, args=(client, addr))
         thread.start()
+
