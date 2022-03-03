230fc4a4c9c97a0f2dfdec41581cb24b4ea6392f
Itay Karavani <itaygrx@gmail.com>
2022-03-03 12:06:21 +0200

    Adding Tests

---
+++ Tests/ServerTest/Server_Test.py
@@ -0,0 +1,16 @@
+import socket
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        serverAddr = ("127.0.0.1",55000)
+        serverSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        serverSock.bind(serverAddr)
+
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
