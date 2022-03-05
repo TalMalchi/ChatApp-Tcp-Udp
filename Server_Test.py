23d1135bbe5e7b1ffc0ee4140bc41947855e2b38
Tal Malchi <malchital@gmail.com>
2022-03-05 13:34:26 +0200

    server tests

--- Server_Test.py
+++ Server_Test.py
@@ -18,11 +18,13 @@ def waitmessage(sock):
 
 
 class ServerTest(TestCase):
-    trd : threading.Thread = None
+    trd: threading.Thread = None
     clientSock = None
     ServerAddr = None
     server_check = None
 
+    # each time we run this test class we connect "default client"
+    # LOGIN+LOGGEDIN test
     @classmethod
     def setUpClass(cls):
         cls.trd = threading.Thread(target=cls.trd, args=[])
@@ -31,64 +33,48 @@ class ServerTest(TestCase):
         cls.clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         cls.clientSock.connect(ServerAddr)
         msg1 = cls.clientSock.recv(1024).decode('utf-8')
-        print(msg1 + "msg")
-        print(cls.trd.getName())
-        print(cls.trd.ident)
-        print(cls.trd.native_id)
-        # cls.assertEqual(msg1, "LOGIN@Connected Succesfully.\n")
         cls.clientSock.send(f"LOGIN@tal@abc".encode('utf-8'))
         msg2 = cls.clientSock.recv(1024).decode('utf-8')
-        print(msg2 + "msg2")
-
+        # cls.assertEqual( "LOGIN@Connected Succesfully.\n", msg1)
         # cls.assertEqual("LOGGEDIN@Logged In Succesfully\n", msg2)
 
+    # when the tests end, we close the client's socket
     @classmethod
     def tearDownClass(cls) -> None:
         cls.clientSock.close()
-        #os.system(f"taskkill /f /PID  {cls.trd.ident}")
-        #os.kill(cls.trd.ident, signal.SIGKILL)
         cls.server_check = None
         cls.trd = None
         cls.clientSock = None
         cls.ServerAddr = None
-
 
     def test_SHOW_FILES(self):
         self.clientSock.send("SHOWFILES@Give me all files in server".encode('utf-8'))
         msg4 = waitmessage(self.clientSock)
         self.assertEqual(msg4.split('@')[0], "SHOWFILES")
 
-
     def test_SHOW_USERS(self):
         self.clientSock.send("SHOWUSERS@".encode('utf-8'))
-        msg3 =waitmessage(self.clientSock)
-        print(msg3)
+        msg3 = waitmessage(self.clientSock)
         self.assertEqual("SHOWUSERS@tal", msg3)
-
 
     def test_MSG(self):
         ServerAddr = ("127.0.0.1", 55000)
         another_clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         another_clientSock.connect(ServerAddr)
         msg = waitmessage(another_clientSock)
-        #self.assertEqual(msg1, "LOGIN@Connected Succesfully.\n")
         another_clientSock.send(f"LOGIN@itay@abc".encode('utf-8'))
         msg1 = waitmessage(another_clientSock)
         another_clientSock.send(f"PMSG@itay@tal@hii".encode('utf-8'))
         msg3 = waitmessage(self.clientSock)
         msg2 = waitmessage(another_clientSock)
-        #print(msg2 + "check msg")
         self.assertEqual("PMSG_S@tal@hii", msg2)
-        self.assertEqual("PMSG@itay@hii",msg3)
+        self.assertEqual("PMSG@itay@hii", msg3)
+        another_clientSock.send("DISCONNECT@itay".encode('utf-8'))
+        msg4 = waitmessage(self.clientSock)
         another_clientSock.close()
 
-
-
-
+    # server's thread
     @classmethod
     def trd(cls):
         cls.server_check = Server.Server()
         cls.server_check.start()
-
-
-
